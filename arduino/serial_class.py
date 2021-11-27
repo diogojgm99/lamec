498cde533885c80ccc3a857be94d96acfdd90f98
diogo <diogomarta10@gmail.com>
2021-11-27 17:47:42 +0000

    Add new functions to class

---
+++ arduino/serial_class.py
@@ -0,0 +1,61 @@
+import serial
+import mariadb
+import sys
+
+# this port address is for the serial tx/rx pins on the GPIO header
+SERIAL_PORT = '/dev/ttyUSB0'
+# be sure to set this to the same rate used on the Arduino
+SERIAL_RATE = 9600
+
+class Database():
+    def __init__(self):
+        try:
+            self.conn = mariadb.connect(
+                user="root",
+                password="root",
+                host="localhost",
+                database="lamec"
+            )
+            self.conn.autocommit = True
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB Platform: {e}")
+            sys.exit(1)
+        self.cur=self.conn.cursor()
+
+    def read_new_tag(self,tag):
+        self.cur.execute("SELECT * FROM tags_registed WHERE tag=?",(tag,))
+        
+        #Se nao houver na db ele insere a nova tag 
+        if not bool(self.cur.fetchall()):
+            print("Insert new tag on database")
+            self.cur.execute("INSERT INTO tags_registed (tag) VALUES (%s)",(tag,))
+        else:
+            print("Tag Founded on database")
+
+    def check_tag_auth(self,tag):
+        self.cur.execute("SELECT tag,name FROM user JOIN tags_registed as tags ON tags.id =user.tag_id WHERE tags.tag=?",(tag,))
+        query=self.cur.fetchall()
+        if query:
+            print("Welcome "+query[0][1])
+        else:
+            print("Non-Authorized User")
+    
+    def close(self):
+        self.conn.close()
+
+
+def main():
+    ser = serial.Serial(SERIAL_PORT, SERIAL_RATE)
+    db = Database()
+
+    while True:
+        reading = ser.readline().decode('utf-8')
+        tag=reading.replace("\r\n","").lstrip(' ')
+        print(tag)
+        db.read_new_tag(tag)
+        db.check_tag_auth(tag)
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
