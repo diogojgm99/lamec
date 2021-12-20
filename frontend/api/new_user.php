2dbcd5443b2ad7b479bf6a1bae0a8eab5ac6d3dc
diogo <diogomarta10@gmail.com>
2021-12-20 21:51:41 +0000

    Fix php on new user

--- frontend/api/new_user.php
+++ frontend/api/new_user.php
@@ -7,32 +7,21 @@ $db = mysqli_connect($servername,$username,$password,$dbname);
 
 // $name = $_POST['name'];
 // $tag = $_POST['tag'];
+$name = "diogo";
 $tag = "E6 16 CE A5";
-$sql = "SELECT * FROM tags_registed WHERE tag='E6 16 CE A5'";
+$sql = "SELECT * FROM tags_registed WHERE tag='$tag'";
 $tag_id = $db->prepare($sql);
 $tag_id->execute();
 $resultSet = $tag_id->get_result();
-echo $resultSet;
-// $tagg = $resultSet->fetch_assoc();
-
 while ($row = $resultSet->fetch_assoc()) {
-    echo $row[1];
+    $results[] = $row;
 }
+$id = json_encode($results[0]['id']);
+echo $id;
 
-// echo $tag_id;
-//$tag = $tag_id->get_result();
-//echo $tag;
-//$value_tag = $tag_id->fetch_assoc();
-//echo $value_tag;
-// $sql = "INSERT INTO users (name,tag_id) VALUES('$name','$tag_id')";
-// echo $sql;
-// if ($db->query($sql) === TRUE)
-// {
-//   echo "Query successful";
-//   echo $sql;
-// }else{
-//   echo "Error: " . $sql . "<br>" . $db->error;
-// }
+$sql = "INSERT INTO user (name,tag_id) VALUES('$name','$id')";
+$new_user = $db->prepare($sql);
+$new_user->execute();
 
 $db->close();
 ?>
