4c23f527187574fa3c01086270c4c058c20a4eec
diogo <diogomarta10@gmail.com>
2021-12-21 20:47:26 +0000

    Fix new_user

--- frontend/api/new_user.php
+++ frontend/api/new_user.php
@@ -5,10 +5,8 @@ $password = "root";
 $dbname = "lamec";
 $db = mysqli_connect($servername,$username,$password,$dbname);
 
-// $name = $_POST['name'];
-// $tag = $_POST['tag'];
-$name = "diogo";
-$tag = "E6 16 CE A5";
+$name = $_POST['name'];
+$tag = $_POST['tag'];
 $sql = "SELECT * FROM tags_registed WHERE tag='$tag'";
 $tag_id = $db->prepare($sql);
 $tag_id->execute();
@@ -17,11 +15,12 @@ while ($row = $resultSet->fetch_assoc()) {
     $results[] = $row;
 }
 $id = json_encode($results[0]['id']);
-echo $id;
 
 $sql = "INSERT INTO user (name,tag_id) VALUES('$name','$id')";
 $new_user = $db->prepare($sql);
 $new_user->execute();
 
 $db->close();
+
+echo "User created successfully!";
 ?>
