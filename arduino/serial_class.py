004a3f674683f753e7d4ec167415d1c8760c33ed
diogo <diogomarta10@gmail.com>
2021-12-25 13:10:09 +0000

    Finish python bridge

--- arduino/serial_class.py
+++ arduino/serial_class.py
@@ -4,9 +4,9 @@ import sys
 from datetime import datetime 
 
 # this port address is for the serial tx/rx pins on the GPIO header
-SERIAL_PORT = '/dev/ttyUSB0'
-# be sure to set this to the same rate used on the Arduino
-SERIAL_RATE = 9600
+# SERIAL_PORT = '/dev/ttyUSB0'
+# # be sure to set this to the same rate used on the Arduino
+# SERIAL_RATE = 9600
 
 class Database():
     def __init__(self):
@@ -38,9 +38,11 @@ class Database():
         query=self.cur.fetchall()
         if query:
             print("Welcome "+query[0][1])
-            self.user_in_out(tag)
             if self.check_count(tag):
+                self.user_in_out(tag)
                 return True
+            else:
+                print("Park is full.")
         else:
             print("Non-Authorized User")
         return False
@@ -51,15 +53,24 @@ class Database():
         query= self.cur.fetchall()
         self.cur.execute("SELECT id from tags_registed WHERE tag=?",(tag,))
         id = self.cur.fetchall()
+        print(id[0][0])
         if not query:
-            self.cur.execute("INSERT INTO in_out (tag_id,time) VALUES (%s)",(id[0],time,))#inserir data de entrada
+            self.cur.execute("INSERT INTO in_out (tag_id,time_in) VALUES (%s,%s)",(id[0][0],time,))#inserir data de entrada
         else:
-            self.cur.execute("UPDATE in_out SET time_out=? WHERE tag_id=?",(time,id[0],))#inserir data de saída
+            self.cur.execute("SELECT time_in FROM in_out WHERE time_out is NULL AND tag_id=?",(id[0][0],))
+            time_in = self.cur.fetchall()
+            time_in = datetime.fromisoformat(time_in[0][0])
+            timestamp1 = datetime.timestamp(time_in)
+            timestamp2 = datetime.timestamp(time)
+            total = timestamp2 - timestamp1
+            cost= round(total/60)
+            self.cur.execute("UPDATE in_out SET time_out=?,total_cost=? WHERE tag_id=? AND time_out is NULL",(time,cost,id[0][0],))#inserir data de saída
     
     def check_count(self,tag):
-        self.cur.execute("SELECT COUNT(*) FROM in_out JOIN tags_registed as tags ON tags.id = in_out.tag_id WHERE tags.tag=? AND time_out IS NULL",(tag,))
+        self.cur.execute("SELECT COUNT(*) FROM in_out WHERE time_out IS NULL")
         query= self.cur.fetchall()
-        if query < 20:
+        print(query[0][0])
+        if query[0][0] < 20:
             return True
         return False
     
@@ -68,19 +79,21 @@ class Database():
 
 
 def main():
-    ser = serial.Serial(SERIAL_PORT, SERIAL_RATE)
+    #ser = serial.Serial(SERIAL_PORT, SERIAL_RATE)
     db = Database()
 
     while True:
-        reading = ser.readline().decode('utf-8')
-        tag=reading.replace("\r\n","").lstrip(' ')
-        print(tag)
+        # reading = ser.readline().decode('utf-8')
+        # tag=reading.replace("\r\n","").lstrip(' ')
+        # print(tag)
+        tag="4E T2 YX A8"
         db.read_new_tag(tag)
         allow_user=db.check_tag_auth(tag)
-        if allow_user:
-            ser.write(b'1')
-        else:
-            ser.write(b'0')
+        break
+        # if allow_user:
+        #     ser.write(b'1')
+        # else:
+        #     ser.write(b'0')
     db.close()
 
 
